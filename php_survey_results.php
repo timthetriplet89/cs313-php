1c6e9045428d9d880d28b7cd51cb99644cb55d53
timthetriplet89 <timthetriplet89@gmail.com>
2016-01-25 00:31:50 -0700

    testing new code... restoring all the functionality hopefully... or was nothing wrong... ?

--- php_survey_results.php
+++ php_survey_results.php
@@ -17,7 +17,7 @@
 
 if (!empty($_POST)) {
     
-    $resultsFile = fopen("results.txt","a+"); 
+    $resultsFile = fopen("results.txt","w+");   //  a+  !!!
 
     $name = "Name: " . $_POST["name"] . "\r\n"; 
     $food = "Food preference: " . $_POST["food"] . "\r\n"; 
@@ -28,26 +28,28 @@ if (!empty($_POST)) {
 
     $surveyText = $name . $food . $color . $book . $movie . $memories; 
 
-    fwrite($resultsFile, $surveyText);
+    fwrite($resultsFile, $surveyText); 
  
      fclose($resultsFile); 
-     }
+     } //
 // Done writing survey results to a file (results.txt) -->
-?>
+?> 
 
-     <!-- Survey Results header  -->
-     <h1>Current Survey Results</h1>
+     <!-- Survey Results header  --> 
+     <h1>Current Survey Results</h1> 
      
 <?php     
     // Read the survey results from the file and display them 
-/*
-if (filesize($resultsFile) != 0) {
+
+//if (filesize($resultsFile) != 0) {
+ 
 $resultsFile = nl2br(file_get_contents("results.txt", true)); 
+ 
 echo $resultsFile; 
-} else {
-    echo "<p>Noone has taken the survey yet.  Perhaps you should " . "<a href=\"survey.html\">start the bandwagon?</a>?</p>";
-}
-*/
+//} else {
+//    echo "<p>Noone has taken the survey yet.  Perhaps you should " . "<a href=\"survey.html\">start the bandwagon?</a>?</p>";
+//}
+
 
 ?>
 
