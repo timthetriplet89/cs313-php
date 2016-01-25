3e3de44917b49a610cffe0b169440d0b44f2af66
timthetriplet89 <timthetriplet89@gmail.com>
2016-01-24 23:17:23 -0700

    added php include statement to survey.html, which should link the CSS style sheet to the survey results page

---
+++ pre_body.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "<html>" . "<head>" . "<meta charset=\"UTF-8\">" . "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" . "<link rel=\"stylesheet\" href=\"css_style_sheet.css\">" . "</head>" . "<body>";
+
+?>
