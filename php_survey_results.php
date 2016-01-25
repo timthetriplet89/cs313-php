e0b29233766b4d4c86820a0d278e22c53d4a89c0
timthetriplet89 <timthetriplet89@gmail.com>
2016-01-24 19:06:00 -0700

    using file_get_contents instead of fgets()

--- php_survey_results.php
+++ php_survey_results.php
@@ -4,8 +4,9 @@
     <body>
       
 <!-- Write survey results to a file (results.txt) -->
+<!-- -------------------------------------------- -->
 <?php 
-
+/*
 $resultsFile = fopen("results.txt","a+"); 
 
     $name = "Name: " . $_POST["name"] . "\r\n"; 
@@ -19,20 +20,25 @@ $resultsFile = fopen("results.txt","a+");
 
     fwrite($resultsFile, $surveyText);
  
- // Why isn't Git recognizing the file changed?
      fclose($resultsFile); ///////////////
 // Done writing survey results to a file (results.txt) -->
 
-/*
+
 if (isset($_POST['food']))
 {
     echo "<p>" . "submit has been registered by post" . "</p>";
     
     // Read the survey results from the file 
     ini_set('auto_detect_line_endings', true);
-    $resultsFile = fopen("results.txt","r") or die("Unable to open file!");    ///////////////
-    $lines = count(file($resultsFile));
-    echo "line count of results file is: " . $lines;
+ * 
+ */
+
+//// Write survey results to a file (results.txt) --> 
+//// -------------------------------------------- --> 
+/* 
+    $resultsFile = fopen("results.txt","r") or die("Unable to open file!");     ///////////////
+    $lines = count(file($resultsFile)); 
+    echo "line count of results file is: " . $lines; 
     //if (filesize($resultsFile) > 0) { 
         
         fread($resultsFile,filesize("results.txt")); 
@@ -41,13 +47,17 @@ if (isset($_POST['food']))
             echo fgets($resultsFile) . "<br>"; 
         //} 
         fclose($resultsFile); 
-        
-    //} else {
-    //    echo "<br>No survey results have been recorded previously.<br>";
-    //}
-}    
- * 
- */
+*/        
+    //} else { 
+    //    echo "<br>No survey results have been recorded previously.<br>"; 
+    //} 
+//} 
+
+//// Try using a different function to get all of the contents of the file as a string.
+//// ------------------------------------------------------------------ --> 
+$resultsFile = nl2br(file_get_contents("results.txt", true));
+echo $resultsFile;
+
 ?>
 
         <p>Parking reserved.
