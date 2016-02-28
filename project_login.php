eebcb2f454d53eb647036e9aa315f0adf39bf791
timthetriplet89 <timthetriplet89@gmail.com>
2016-02-27 20:06:59 -0700

    Testing connection to database, in project_login.php

--- project_login.php
+++ project_login.php
@@ -1,5 +1,13 @@
 <?php
     session_start();
+    
+    require("dbConnector.php"); 
+
+    $db = loadDatabase(); 
+    
+    // For this assignment (week 5) we will demonstrate displaying data for one user,
+    //  instead of processing the log-in information from the previous page.
+    
 ?>
 
 <!--  By: Timothy Steele -->
@@ -12,13 +20,33 @@
         <link rel="stylesheet" href="css_style_sheet.css">
     </head>
     
-    <?php
-    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-echo "<br><br>";
-echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n"; 
-    ?>
+    <body>
+        
+    <!--  Check to make sure the form was submitted
+          -- See "scripture_submit.php" from my team activity.
+          --  if (isset($_POST['book']) && isset($_POST['chapter']) && isset($_POST['verse']))   -->    
     
+    <!-- hard-code in the user we're logging in as (to start off with).
+    Next step will be implementing the password logging in -->
+    <?php
+    require("dbConnector.php"); 
+
+    $db = loadDatabase();
+    
+//    // Next thing to test:
+//    $query = $db->prepare("SELECT * FROM connections WHERE agentID = 1");
+//    $query->execute();
+    
+    //$my_connections = $db->query
+        
+//   foreach ($my_connections as $connection) {     
+////        $connection_recipientID = $db->query("SELECT recipientID");
+////        $connection_user = $db->query("SELECT * WHERE $connectionID")
+//echo <<<HTML
+//            <div>RecipientID = {$connection['recipientID']}.</div>
+//HTML;
+//    }
+    
+    ?>
+    </body>
 </html>
---
+++ setLocalDatabaseCredentials.php
@@ -0,0 +1,6 @@
+<?php
+	$dbHost = 'localhost';
+	$dbUsername = 'root';
+	$password = '';
+	$dbname = 'cs313';
+?>
