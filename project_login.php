17b370184a2c9ca8e2f3652db3adf38fcae2d99a
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-05 12:56:23 -0700

    More troubleshooting

--- project_login.php
+++ project_login.php
@@ -9,12 +9,17 @@
 
     $db = loadDatabase(); 
     
-    $queryForID = $db->prepare('SELECT userID FROM users WHERE username = ' . $_SESSION['username']);
+    $queryForID = $db->prepare('SELECT userID FROM users WHERE username = \'' . $_SESSION['username'] . '\'');
     $queryForID->execute();
     
     $_SESSION['userID'] = $queryForID->fetch();
-    echo $queryForID->fetch();
-    echo $_SESSION['userID'];
+    //echo $queryForID->fetch();
+    if (isset($_SESSION['userID'])) {
+        echo $_SESSION['userID'];
+    } else {
+        echo 'No session variable set for userID';
+    }
+    
     
     
     // For this assignment (week 5) we will demonstrate displaying data for one user,
