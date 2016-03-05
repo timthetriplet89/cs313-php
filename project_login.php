0b0c4e321762f9851d128c8457fc65c364d0867b
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-05 13:36:03 -0700

    ... ... ...

--- project_login.php
+++ project_login.php
@@ -12,15 +12,13 @@
     $queryForID = $db->prepare('SELECT userID FROM users WHERE username = \'' . $_SESSION['username'] . '\'');
     $queryForID->execute();
     
-    $_SESSION['userID'] = $queryForID->fetch();
+    $_SESSION['userID'] = $queryForID->fetch(PDO::FETCH_ASSOC);
     //echo $queryForID->fetch();
     if (isset($_SESSION['userID'])) {
         echo $_SESSION['userID'];
     } else {
         echo 'No session variable set for userID';
     }
-    
-    
     
     // For this assignment (week 5) we will demonstrate displaying data for one user,
     //  instead of processing the log-in information from the previous page.
