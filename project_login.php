183b4e63b0becddb1988a404c3610dd8718d0302
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-05 12:50:50 -0700

    ...

--- project_login.php
+++ project_login.php
@@ -9,17 +9,16 @@
 
     $db = loadDatabase(); 
     
-    //$queryForID = $db->prepare('SELECT userID FROM users WHERE username = \"jensen\"');
-    //$queryForID->execute();
+    $queryForID = $db->prepare('SELECT userID FROM users WHERE username = ' . $_SESSION['username']);
+    $queryForID->execute();
     
-    //$_SESSION['userID'] = $queryForID->fetch();
-    //echo $queryForID->fetch();
-    //echo $_SESSION['userID'];
+    $_SESSION['userID'] = $queryForID->fetch();
+    echo $queryForID->fetch();
+    echo $_SESSION['userID'];
     
     
     // For this assignment (week 5) we will demonstrate displaying data for one user,
     //  instead of processing the log-in information from the previous page.
-    
 ?>
 
 <!--  By: Timothy Steele -->
