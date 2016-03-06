83dd57556a270a69ebc59a66443d40b745581be5
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-05 23:57:24 -0700

    Cleaned up code

--- project_login.php
+++ project_login.php
@@ -3,7 +3,6 @@
     
     // First check to see if the username is set! ...  Add that check.
     $_SESSION['username'] = $_POST['username'];
-    //echo $_SESSION['username'];
     
     require("dbConnector.php"); 
 
@@ -11,15 +10,10 @@
     
     $queryForID = $db->prepare("SELECT userID FROM users WHERE username ='" . $_SESSION['username'] . "'");
     $queryForID->execute();
-    
     $result = $queryForID->fetch();
     
-    // Under test...
-   //['userID'] = $result['userID'];    
     $_SESSION['agentID'] = $result['userID'];
     
-    // For this assignment (week 5) we will demonstrate displaying data for one user,
-    //  instead of processing the log-in information from the previous page.
 ?>
 
 <!--  By: Timothy Steele -->
@@ -37,8 +31,6 @@
     <?php
 
     try {
-
-        echo "<p>Inside the try statement...</p>";
         
         $users = $db->prepare('SELECT q.text, q.author, u.userID, u.name' . 
                               ' FROM connections AS c INNER JOIN users AS u' .
@@ -49,7 +41,6 @@
         $users->execute();
         
         while ($row = $users->fetch(PDO::FETCH_ASSOC)) {
-            //echo '<p>Reading a row:</p>';
             echo '<a href=\'project_list_quotes.php?userID=' . $row['userID'] . '\'>' . $row['name'] . '</a><br>';
         }
         
