ecbacb60992203e8c1b11e3653dc7cde21e38c3b
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-06 22:47:26 -0700

    querying for quotes of friend connections, on project_list_quotes.php

--- project_list_quotes.php
+++ project_list_quotes.php
@@ -3,13 +3,11 @@
 
     if (isset($_GET['userID'])) {
        $_SESSION['user_connection'] = $_GET['userID'];
-    } 
-//    else {
-//        require("pre_body.html");
-//        echo "This person prefers to keep an air of mystery about him.";
-//        require("post_body.html");
-//    }
-
+    }
+    
+    require("dbConnector.php"); 
+    $db = loadDatabase(); 
+    
 ?>
 
 <!--  By: Timothy Steele -->
@@ -25,16 +23,31 @@
     <body>
 
    <?php 
-    if (isset($_SESSION['user_connection'])) {
-        echo $_SESSION['user_connection'];
-    }
-
-?> 
+    if (!isset($_SESSION['user_connection'])) {
+        echo "This person prefers to keep an air of mystery about him.";
+    } else {
+        try {
+            
+            $quotes = $db->prepare('SELECT q.text, q.author' .
+                    'FROM users AS u' .
+                    'INNER JOIN user_quote AS srqt' .
+                    'ON u.userID = srqt.userID' .
+                    'INNER JOIN quotes q' .
+                    'ON srqt.quoteID = q.quoteID' .
+                    'WHERE u.userID = ' . $_SESSION['user_connection']);
+            
+            $quotes->execute();
+            while ($row = $quotes->fetch(PDO::FETCH_ASSOC)) {
+                echo '<p>"' . $row['text'] . '"   -' . $row['author'] . '</p>';
+            }
         
+        } catch (PDOException $ex) {
+            echo "Error with DB. Details: $ex";
+            die();
+        }
         
-        
-        
-        
+    } 
+?>   
         
   </body>
 </html> 
