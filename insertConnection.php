215229f1fc682cd38156b16fbc7dd482db42918b
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-09 09:41:49 -0700

    ...

--- insertConnection.php
+++ insertConnection.php
@@ -1,9 +1,6 @@
 <?php
 
     session_start();
-              
-    require("dbConnector.php");
-    $db = loadDatabase();
     
     $usernameToAdd = $_POST['enterUsername'];
     $agentID = $_SESSION['agentID'];
@@ -13,33 +10,40 @@
        
     try {
         
+        echo "before database connevction setup<br>";
+            
+        require("dbConnector.php");
+        $db = loadDatabase();
+        
+        echo "after database connection setup<br>";
+        
         // first check to see if this username is in the system!
         $query1 = 'SELECT COUNT( * ) AS total FROM users WHERE username = ' . $usernameToAdd;
         $query1->execute();
         $row = $query1->fetch(PDO::FETCH_ASSOC);
         $numUsername = $row['total'];
         echo "numUsername: <br>";
-        echo "$numUsername
+        echo $numUsername;
     
         if ($numUsername == 1) {
             // Get the userID for the username to be added!  (See first part of login_page.php -- where I get logged in user's userID
-            $query2 = $db->prepare("SELECT userID FROM users WHERE username ='" . $usernameToAdd . "'");  
+            $query2 = $db->prepare('SELECT userID FROM users WHERE username =\'' . $usernameToAdd . '\'');  
             $query2->execute();
             $userID_ToAdd = $query2->fetch(PDO::FETCH_ASSOC);
            
-//            // Insert a connection
-//            $query3 = 'INSERT INTO connections(agentID, recipientID) VALUES (:agentID, :recipientID)';
-//            $statement3 = $db->prepare($query3);
-//            $statement3->bindParam(':agentID', $agentID);
-//            $statement3->bindParam(':recipientID', $userID_ToAdd);
-//            $statement3->execute();
-//            
-//            // Insert a reverse connection
-//            $query3 = 'INSERT INTO connections(agentID, recipientID) VALUES (:agentID, :recipientID)';
-//            $statement3 = $db->prepare($query3);
-//            $statement3->bindParam(':agentID', $userID_ToAdd);
-//            $statement3->bindParam(':recipientID', $agentID);  
-//            $statement3->execute();
+            // Insert a connection
+            $query3 = 'INSERT INTO connections(agentID, recipientID) VALUES (:agentID, :recipientID)';
+            $statement3 = $db->prepare($query3);
+            $statement3->bindParam(':agentID', $agentID);
+            $statement3->bindParam(':recipientID', $userID_ToAdd);
+            $statement3->execute();
+            
+            // Insert a reverse connection
+            $query3 = 'INSERT INTO connections(agentID, recipientID) VALUES (:agentID, :recipientID)';
+            $statement3 = $db->prepare($query3);
+            $statement3->bindParam(':agentID', $userID_ToAdd);
+            $statement3->bindParam(':recipientID', $agentID);  
+            $statement3->execute();
         }
     }
     catch (Exception $ex)
@@ -54,3 +58,5 @@
 //die();
 
 ?>
+                
+                
