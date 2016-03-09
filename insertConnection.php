4b4c317041cb792299b76a2b984de957edd4aad9
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-09 10:37:43 -0700

    .....

--- insertConnection.php
+++ insertConnection.php
@@ -10,18 +10,18 @@
        
     try {
         
-        //echo "before database connevction setup<br>";
+        echo "before database connevction setup<br>";
         require("dbConnector.php");
         $db = loadDatabase();
-        //echo "after database connection setup<br>";
+        echo "after database connection setup<br>";
         
-        // first check to see if this username is in the system!
-        $query1 = 'SELECT COUNT( * ) AS total FROM users WHERE username = ' . $usernameToAdd;
-        $query1->execute();
-        $row = $query1->fetch(PDO::FETCH_ASSOC);
-        $numUsername = $row['total'];
-        //echo "numUsername: <br>";
-        //echo $numUsername;
+//        // first check to see if this username is in the system!
+//        $query1 = 'SELECT COUNT( * ) AS total FROM users WHERE username = ' . $usernameToAdd;
+//        $query1->execute();
+//        $row = $query1->fetch(PDO::FETCH_ASSOC);
+//        $numUsername = $row['total'];
+//        //echo "numUsername: <br>";
+//        //echo $numUsername;
     
 //        if ($numUsername == 1) {
 //            // Get the userID for the username to be added!  (See first part of login_page.php -- where I get logged in user's userID
