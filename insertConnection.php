3d12bd9723e32ee7ea33fb5032fd3fe3fb970852
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-09 09:08:45 -0700

    Testing a few lines of code at a time

--- insertConnection.php
+++ insertConnection.php
@@ -1,41 +1,46 @@
 <?php
 
     session_start();
+        
+    $usernameToAdd = $_POST['enterUsername'];
+    $agentID = $_SESSION['agentID'];
     
-        $usernameToAdd = $_POST['enterUsername'];
-        $agentID = $_SESSION['agentID'];
+    echo "<p>usernameToAdd: $usernameToAdd</p>"
+       . "<p>agentID: $agentID</p>";
        
     try {
         
-        require("dbConnector.php");
-        $db = loadDatabase();
+        echo "Empty Try Statement";
         
-        // first check to see if this username is in the system!
-        $query1 = 'SELECT COUNT( * ) AS total FROM users WHERE username = ' . $usernameToAdd;
-        $query1->execute();
-        $row = $query1->fetch(PDO::FETCH_ASSOC);
-        $numUsername = $row['total'];
-    
-        if ($numUsername == 1) {
-            // Get the userID for the username to be added!  (See first part of login_page.php -- where I get logged in user's userID
-            $query2 = $db->prepare("SELECT userID FROM users WHERE username ='" . $usernameToAdd . "'");  
-            $query2->execute();
-            $userID_ToAdd = $query2->fetch(PDO::FETCH_ASSOC);
-           
-            // Insert a connection
-            $query3 = 'INSERT INTO connections(agentID, recipientID) VALUES (:agentID, :recipientID)';
-            $statement3 = $db->prepare($query3);
-            $statement3->bindParam(':agentID', $agentID);
-            $statement3->bindParam(':recipientID', $userID_ToAdd);
-            $statement3->execute();
-            
-            // Insert a reverse connection
-            $query3 = 'INSERT INTO connections(agentID, recipientID) VALUES (:agentID, :recipientID)';
-            $statement3 = $db->prepare($query3);
-            $statement3->bindParam(':agentID', $userID_ToAdd);
-            $statement3->bindParam(':author', $agentID);  
-            $statement3->execute();
-        }
+//    require("dbConnector.php");
+//    $db = loadDatabase();
+//        
+//        // first check to see if this username is in the system!
+//        $query1 = 'SELECT COUNT( * ) AS total FROM users WHERE username = ' . $usernameToAdd;
+//        $query1->execute();
+//        $row = $query1->fetch(PDO::FETCH_ASSOC);
+//        $numUsername = $row['total'];
+//    
+//        if ($numUsername == 1) {
+//            // Get the userID for the username to be added!  (See first part of login_page.php -- where I get logged in user's userID
+//            $query2 = $db->prepare("SELECT userID FROM users WHERE username ='" . $usernameToAdd . "'");  
+//            $query2->execute();
+//            $userID_ToAdd = $query2->fetch(PDO::FETCH_ASSOC);
+//           
+//            // Insert a connection
+//            $query3 = 'INSERT INTO connections(agentID, recipientID) VALUES (:agentID, :recipientID)';
+//            $statement3 = $db->prepare($query3);
+//            $statement3->bindParam(':agentID', $agentID);
+//            $statement3->bindParam(':recipientID', $userID_ToAdd);
+//            $statement3->execute();
+//            
+//            // Insert a reverse connection
+//            $query3 = 'INSERT INTO connections(agentID, recipientID) VALUES (:agentID, :recipientID)';
+//            $statement3 = $db->prepare($query3);
+//            $statement3->bindParam(':agentID', $userID_ToAdd);
+//            $statement3->bindParam(':recipientID', $agentID);  
+//            $statement3->execute();
+//        }
     }
     catch (Exception $ex)
 {
@@ -45,7 +50,7 @@
 	die();
 }
 
-header("Location: mypage.php");
-die();
+//header("Location: mypage.php");
+//die();
 
 ?>
