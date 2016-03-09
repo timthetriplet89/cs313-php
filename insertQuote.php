79633d1d757c2183cb76557c608986f5f39b8636
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-08 23:52:34 -0700

    adding added redirect to insertQuote.php

--- insertQuote.php
+++ insertQuote.php
@@ -4,18 +4,17 @@
 
    
         //if(isset($_POST['text']) & isset($_POST['author'])) {
-    
-    try {
-        
-        require("dbConnector.php"); 
-        $db = loadDatabase(); 
-        
         $text = $_POST['quoteText'];
         $author =  $_POST['quoteAuthor'];  
         $agentID = $_SESSION['agentID'];
         
         echo "quoteText: $quoteText\n";
         echo "quoteAuthor: $quoteAuthor\n";
+    
+    try {
+        
+        require("dbConnector.php"); 
+        $db = loadDatabase(); 
     
         // Insert quote submitted by the user into the 'quotes' table
         $query = 'INSERT INTO quotes(text, author) VALUES (:text, :author)';
@@ -39,22 +38,8 @@
 	echo "Error with DB. Details: $ex";
 	die();
 }
+
+header("Location: mypage.php");
+die();
+
 ?>
-
-<!--  By: Timothy Steele -->
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Me & Quotes</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css_style_sheet.css">
-    </head>
-    
-    <body>
-
-        <header>New quote went to database (fingers crossed)</header>
-
-
-    </body>
-</html>
