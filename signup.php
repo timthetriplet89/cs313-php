e9c81b62fa1576bdfb814ec32d3d199af717cb15
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-10 21:43:35 -0700

    .....

--- signup.php
+++ signup.php
@@ -1,14 +1,16 @@
 <?php
-	require_once("dbConnector.php");
-	require_once("password.php");
+	require("dbConnector.php");  //  require_once("dbConnector.php")
+	require("password.php"); // require_once("password.php");
 
-	if($_POST) { 
-		$user_create_query = $db->prepare("INSERT INTO users (name, taglineID, username, password) VALUES (1,\'no_name\', :username, :password)"); 
-		$user_create_query->bindParam(':username', $_POST['username']); 
-		$user_create_query->bindParam(':password', password_hash($_POST['password'], 'abc123')); 
-		$user_create_query->execute(); 
- 
-		header("Location: signin.php"); 
+	if($_POST) {
+            
+            echo 'made it into POST';
+//		$user_create_query = $db->prepare("INSERT INTO users (name, taglineID, username, password) VALUES (1,\'no_name\', :username, :password)"); 
+//		$user_create_query->bindParam(':username', $_POST['username']); 
+//		$user_create_query->bindParam(':password', password_hash($_POST['password'], 'abc123')); 
+//		$user_create_query->execute(); 
+// 
+//		header("Location: signin.php"); 
 	} 
 ?>
 <!DOCTYPE html>
