f5228e912cb889058806009602ccbb1384fd4a1a
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-10 22:09:24 -0700

    ....................

--- signup.php
+++ signup.php
@@ -5,10 +5,10 @@
 	if($_POST) {
             
 //            echo 'made it into POST';
-//		$user_create_query = $db->prepare("INSERT INTO users (name, taglineID, username, password) VALUES (1,\'no_name\', :username, :password)"); 
-//		$user_create_query->bindParam(':username', $_POST['username']); 
-//		$user_create_query->bindParam(':password', password_hash($_POST['password'], 'abc123')); 
-//		$user_create_query->execute(); 
+		$user_create_query = $db->prepare("INSERT INTO users (name, taglineID, username, password) VALUES (1,\'no_name\', :username, :password)"); 
+		$user_create_query->bindParam(':username', $_POST['username']); 
+		$user_create_query->bindParam(':password', password_hash($_POST['password'], 'abc123')); 
+		$user_create_query->execute(); 
 // 
 //		header("Location: signin.php"); 
 	} 
