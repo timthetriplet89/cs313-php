0175e5927cb617509afb9c85a4ec7d2f86ee90fe
timthetriplet89 <timthetriplet89@gmail.com>
2016-03-11 23:22:42 -0700

    Inserting a user into users, from signup.php page

--- signup.php
+++ signup.php
@@ -3,14 +3,14 @@
 	require("password.php"); // require_once("password.php");
 
 	if($_POST) {
-            
-//            echo 'made it into POST';
-		$user_create_query = $db->prepare("INSERT INTO users (name, taglineID, username, password) VALUES (1,\'no_name\', :username, :password)"); 
-		$user_create_query->bindParam(':username', $_POST['username']); 
-		$user_create_query->bindParam(':password', password_hash($_POST['password'], 'abc123')); 
-		$user_create_query->execute(); 
-// 
-//		header("Location: signin.php"); 
+            // First, create a new user in the users table (with a name, username, and password)
+            //$user_create_query = $db->prepare("INSERT INTO users (name, taglineID, username, password) VALUES (:name, :taglineID, :username, :password)"); 
+            $user_create_query = $db->prepare("INSERT INTO users (name, username, password) VALUES (:name, :username, :password)"); 
+            $user_create_query->bindParam(':name', $_POST['_name']);
+            $user_create_query->bindParam(':username', $_POST['username']);   // Add the rest of the bindParam's !!!
+            $user_create_query->bindParam(':password', password_hash($_POST['password'], 'abc123')); 
+            $user_create_query->execute();
+//          header("Location: signin.php"); 
 	} 
 ?>
 <!DOCTYPE html>
@@ -20,18 +20,30 @@
 	<meta charset="utf-8">
 </head>
 <body>
-	<form method="post">
-		<div>
-			<label for="username">Username</label>
-			<input type="text" name="username" id="username"/>
-		</div>
-		<div>
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password"/>
-		</div>
-		<div>
-			<button type="submit">Sign Up</button>
-		</div>
+        <form method="post">
+            <div>
+                <label for="_name">Your Name:</label>
+                <input type="text" name="_name" id="_name"
+            <div>
+                <label for="quote_text">A Favorite Quote</label>
+                <input type="text" name="_text" id="_text">
+            </div>
+            <div>
+                <label for="quote_author">And The Author</label>
+                <input type="text" name="_author" id="_author">
+            </div>
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username"/>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password"/>
+            </div>
+            <div>
+                <button type="submit">Sign Up</button>
+            </div>
+                
 	</form>
 </body>
 </html>
