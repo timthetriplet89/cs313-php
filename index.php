d39d094f17768ee0eebb03ac58ca9d81f048e6b5
timthetriplet89 <timthetriplet89@gmail.com>
2016-04-30 17:03:34 -0600

    Changing Home page

--- index.php
+++ index.php
@@ -7,19 +7,41 @@ and open the template in the editor.
 
 <html>
     <head>
-        <title>Greetings</title>
+        <title>Tim Steele - CS 313</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css_style_sheet.css">
         <script>
+//            function myFunction() {
+//                //document.getElementById("body_element").getAttribute("background") = 
+//                document.body.style.background = "#8CBCE2";
+//            }
 
+            function testJavaScript() {
+                alert("Yes, JavaScript seems to be working just fine today!")
+            }
         </script>
     </head>
     <body id="index" class="body">
         
         <div id="center_div">
-            <h1><?php echo "Hello PHP World." ?></h1><br>
+            <h1>Welcome!</h1><br>
             
+            <img src="me.jpg" alt="Timothy" id="selfie"/>
+            
+            <p>My name is Timothy Steele, and I am a computer science major at BYU-Idaho.  
+                I enjoy problem-solving and learning new things.</p>
+            
+            <p>I also like to run, watch the old movies my friends didn't know existed, do family history, and spend time with family.</p>
+            
+            <p>I look forward to gaining more experience with web development on 
+            the server side!</p>
+            
+            <h2>Please see what I'm learning at my
+                <a href="list_assignments.html" 
+                 onmouseover="myFunction()"> assignments page.</a></h2>
+            
+            <p>To test whether or not JavaScript is working today, <button onclick="testJavaScript()">Click Here</button></p>
         </div>
         
     </body>
