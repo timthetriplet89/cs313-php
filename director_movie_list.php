987b4ec2b99bb4aba119565ca78c3ac720b59033
timthetriplet89 <timthetriplet89@gmail.com>
2016-05-23 11:08:45 -0700

    .............

--- director_movie_list.php
+++ director_movie_list.php
@@ -5,23 +5,23 @@
        $_SESSION['director_id'] = $_GET['director_id'];
     }
     
-    // Load The Database
-    require("dbConnector.php"); 
-    $db = loadDatabase();
-    
-    // Get the Director's Name
-    $statement1 = "SELECT name FROM directors WHERE id = " . $_SESSION['director_id'];
-    $queryDirectorName = $db->prepare($statement1);
-    $queryDirectorName->execute();
-    $director = $queryDirectorName->fetch(PDO::FETCH_ASSOC);
-    $_SESSION['director_name'] = $director['name'];
+//    // Load The Database
+//    require("dbConnector.php"); 
+//    $db = loadDatabase();
+//    
+//    // Get the Director's Name
+//    $statement1 = "SELECT name FROM directors WHERE id = " . $_SESSION['director_id'];
+//    $queryDirectorName = $db->prepare($statement1);
+//    $queryDirectorName->execute();
+//    $director = $queryDirectorName->fetch(PDO::FETCH_ASSOC);
+//    $_SESSION['director_name'] = $director['name'];
 ?>
 
 <!--  By: Timothy Steele -->
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php echo $_SESSION['director_name'] ?></title>
+        <title>Director Info</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css_style_sheet.css">
@@ -29,7 +29,7 @@
     
     <body>
         
-        <header><?php echo $_SESSION['director_name'] ?></header><br>
+        <header>Director Info</header><br>
    
     <?php
     
@@ -37,26 +37,25 @@
         require("dbConnector.php");
         $db = loadDatabase();
 
-                echo "director_id = " . $_SESSION['director_id'] . '<br>';
-        echo "director name = " . $_SESSION['director_name']  . '<br>';
+        echo "director_id = " . $_SESSION['director_id'] . '<br>';
         
     try {
         
-        $statement = "SELECT title, id FROM movies WHERE director_id = " . $_SESSION['director_id']; 
+        $statement = "SELECT title, director_id, id FROM movies WHERE director_id = " . $_SESSION['director_id']; 
         $queryMovies = $db->prepare($statement);
         $queryMovies->execute();
         
         while ($movie = $queryMovies->fetch(PDO::FETCH_ASSOC)) {
             $url = "\"http://php-steele2.rhcloud.com/single_movie.php?movie_id=" . $movie['id']. "\"";
-            echo '<p><a href=' . $url . '>' . $$movie['title'] . '</a></p>';
+            echo '<p><a href=' . $url . '>' . $movie['title'] . '</a></p>';
         }
         
     } catch (PDOException $ex) {
         echo "Error with DB. Details: $ex";
         die();
-    } 
-     
-    ?>
+    }   
+        
+    ?>  
         
     </body>
 </html>
