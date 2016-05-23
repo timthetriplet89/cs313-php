13d140aa03a06e2e2e67c50dc6f7f8c03bceca22
timthetriplet89 <timthetriplet89@gmail.com>
2016-05-23 10:18:02 -0700

    .......

--- director_movie_list.php
+++ director_movie_list.php
@@ -33,22 +33,22 @@
    
     <?php
 
-//    try {
-//        
-//        $statement = "SELECT name, id FROM directors"; 
-//        $queryDirectors = $db->prepare($statement); 
-//        $queryDirectors->execute(); 
-//        
-//            while ($director = $queryDirector->fetch(PDO::FETCH_ASSOC)) {
-//                $url = "\"http://php-steele2.rhcloud.com/director_movie_list.php?director_id=" . $director['id']. "\"";
-//                echo '<p><a href=' . $url . '>' . $director['name'] . '</a></p>';
-//            }
-//            
-//    } catch (PDOException $ex) {
-//        echo "Error with DB. Details: $ex";
-//        die();
-//    } 
-    
+    try {
+        
+        $statement = "SELECT title, id FROM movies WHERE director_id = " . $_SESSION['director_name']; 
+        $queryMovies = $db->prepare($statement); 
+        $queryMovies->execute(); 
+        
+        while ($movie = $queryMovies->fetch(PDO::FETCH_ASSOC)) {
+            $url = "\"http://php-steele2.rhcloud.com/single_movie.php?movie_id=" . $movie['id']. "\"";
+            echo '<p><a href=' . $url . '>' . $$movie['title'] . '</a></p>';
+        }
+            
+    } catch (PDOException $ex) {
+        echo "Error with DB. Details: $ex";
+        die();
+    } 
+     
     ?>
         
     </body>
